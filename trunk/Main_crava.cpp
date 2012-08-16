376d1cc63228e068493a1821beb5319f7d58394d
Andreas B. Lindblad <andreas.b.lindblad@gmail.com>
2012-08-16 09:25:32 +0200

    on_densityFile1LineEdit_editingFinished - calls the method densityFile() to update the tree with a file

--- trunk/Main_crava.cpp
+++ trunk/Main_crava.cpp
@@ -4133,6 +4133,9 @@ void Main_crava::vsFile(const QString &value){
 	}
 }
 
+void Main_crava::on_densityFile1LineEdit_editingFinished(){
+	densityFile(densityFile1LineEdit->text());
+};//update the XML three with the file if it is correct, autocomplete would be nice, prior model density file
 void Main_crava::densityFile(const QString &value){
 	// should remove the constant from the tree
 	if (standard->StandardStrings::fileExists(value)){
